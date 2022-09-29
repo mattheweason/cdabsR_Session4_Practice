b120f748bf61307026cf58af1d075331b4fda905
mattheweason <matthew.eason@som.umaryland.edu>
2022-09-29 18:11:46 +0000

    updated my repo with new info in the read me

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cdabsR_Session4_Practice
 learning to connect to GitHub from R Studio Cloud and push changes
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+git_sitrep()
+
+
+# Create a personal access token
+
+create_github_token()
+
+# Add token to get credentials store
+gitcreds::gitcreds
+
---
+++ connection with Git.R
@@ -0,0 +1,388 @@
+
+R version 4.2.1 (2022-06-23) -- "Funny-Looking Kid"
+Copyright (C) 2022 The R Foundation for Statistical Computing
+Platform: x86_64-pc-linux-gnu (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+> install.packages("usethis")
+Installing package into ‘/cloud/lib/x86_64-pc-linux-gnu-library/4.2’
+(as ‘lib’ is unspecified)
+also installing the dependencies ‘mime’, ‘R6’, ‘askpass’, ‘credentials’, ‘openssl’, ‘sys’, ‘zip’, ‘gitcreds’, ‘httr’, ‘ini’, ‘magrittr’, ‘cli’, ‘clipr’, ‘crayon’, ‘curl’, ‘desc’, ‘fs’, ‘gert’, ‘gh’, ‘glue’, ‘jsonlite’, ‘lifecycle’, ‘purrr’, ‘rappdirs’, ‘rlang’, ‘rprojroot’, ‘rstudioapi’, ‘whisker’, ‘withr’, ‘yaml’
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/mime_0.12.tar.gz'
+Content type 'application/x-gzip' length 37125 bytes (36 KB)
+==================================================
+  downloaded 36 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/R6_2.5.1.tar.gz'
+Content type 'application/x-gzip' length 82477 bytes (80 KB)
+==================================================
+  downloaded 80 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/askpass_1.1.tar.gz'
+Content type 'application/x-gzip' length 23822 bytes (23 KB)
+==================================================
+  downloaded 23 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/credentials_1.3.2.tar.gz'
+Content type 'application/x-gzip' length 172050 bytes (168 KB)
+==================================================
+  downloaded 168 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/openssl_2.0.3.tar.gz'
+Content type 'application/x-gzip' length 1400287 bytes (1.3 MB)
+==================================================
+  downloaded 1.3 MB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/sys_3.4.tar.gz'
+Content type 'application/x-gzip' length 55135 bytes (53 KB)
+==================================================
+  downloaded 53 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/zip_2.2.1.tar.gz'
+Content type 'application/x-gzip' length 624790 bytes (610 KB)
+==================================================
+  downloaded 610 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/gitcreds_0.1.2.tar.gz'
+Content type 'application/x-gzip' length 95550 bytes (93 KB)
+==================================================
+  downloaded 93 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/httr_1.4.4.tar.gz'
+Content type 'application/x-gzip' length 498400 bytes (486 KB)
+==================================================
+  downloaded 486 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/ini_0.3.1.tar.gz'
+Content type 'application/x-gzip' length 13126 bytes (12 KB)
+==================================================
+  downloaded 12 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/magrittr_2.0.3.tar.gz'
+Content type 'application/x-gzip' length 234989 bytes (229 KB)
+==================================================
+  downloaded 229 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/cli_3.4.1.tar.gz'
+Content type 'application/x-gzip' length 1306037 bytes (1.2 MB)
+==================================================
+  downloaded 1.2 MB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/clipr_0.8.0.tar.gz'
+Content type 'application/x-gzip' length 50340 bytes (49 KB)
+==================================================
+  downloaded 49 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/crayon_1.5.1.tar.gz'
+Content type 'application/x-gzip' length 156295 bytes (152 KB)
+==================================================
+  downloaded 152 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/curl_4.3.2.tar.gz'
+Content type 'application/x-gzip' length 905296 bytes (884 KB)
+==================================================
+  downloaded 884 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/desc_1.4.2.tar.gz'
+Content type 'application/x-gzip' length 317043 bytes (309 KB)
+==================================================
+  downloaded 309 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/fs_1.5.2.tar.gz'
+Content type 'application/x-gzip' length 760048 bytes (742 KB)
+==================================================
+  downloaded 742 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/gert_1.9.0.tar.gz'
+Content type 'application/x-gzip' length 2638965 bytes (2.5 MB)
+==================================================
+  downloaded 2.5 MB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/gh_1.3.1.tar.gz'
+Content type 'application/x-gzip' length 94078 bytes (91 KB)
+==================================================
+  downloaded 91 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/glue_1.6.2.tar.gz'
+Content type 'application/x-gzip' length 157185 bytes (153 KB)
+==================================================
+  downloaded 153 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/jsonlite_1.8.0.tar.gz'
+Content type 'application/x-gzip' length 1158740 bytes (1.1 MB)
+==================================================
+  downloaded 1.1 MB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/lifecycle_1.0.2.tar.gz'
+Content type 'application/x-gzip' length 106211 bytes (103 KB)
+==================================================
+  downloaded 103 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/purrr_0.3.4.tar.gz'
+Content type 'application/x-gzip' length 426960 bytes (416 KB)
+==================================================
+  downloaded 416 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/rappdirs_0.3.3.tar.gz'
+Content type 'application/x-gzip' length 47588 bytes (46 KB)
+==================================================
+  downloaded 46 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/rlang_1.0.6.tar.gz'
+Content type 'application/x-gzip' length 1923948 bytes (1.8 MB)
+==================================================
+  downloaded 1.8 MB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/rprojroot_2.0.3.tar.gz'
+Content type 'application/x-gzip' length 100991 bytes (98 KB)
+==================================================
+  downloaded 98 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/rstudioapi_0.14.tar.gz'
+Content type 'application/x-gzip' length 290679 bytes (283 KB)
+==================================================
+  downloaded 283 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/whisker_0.4.tar.gz'
+Content type 'application/x-gzip' length 66029 bytes (64 KB)
+==================================================
+  downloaded 64 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/withr_2.5.0.tar.gz'
+Content type 'application/x-gzip' length 225454 bytes (220 KB)
+==================================================
+  downloaded 220 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/yaml_2.3.5.tar.gz'
+Content type 'application/x-gzip' length 286243 bytes (279 KB)
+==================================================
+  downloaded 279 KB
+
+trying URL 'http://rspm/default/__linux__/focal/latest/src/contrib/usethis_2.1.6.tar.gz'
+Content type 'application/x-gzip' length 789524 bytes (771 KB)
+==================================================
+  downloaded 771 KB
+
+* installing *binary* package ‘mime’ ...
+* DONE (mime)
+* installing *binary* package ‘R6’ ...
+* DONE (R6)
+* installing *binary* package ‘sys’ ...
+* DONE (sys)
+* installing *binary* package ‘zip’ ...
+* DONE (zip)
+* installing *binary* package ‘gitcreds’ ...
+* DONE (gitcreds)
+* installing *binary* package ‘ini’ ...
+* DONE (ini)
+* installing *binary* package ‘magrittr’ ...
+* DONE (magrittr)
+* installing *binary* package ‘cli’ ...
+* DONE (cli)
+* installing *binary* package ‘clipr’ ...
+* DONE (clipr)
+* installing *binary* package ‘crayon’ ...
+* DONE (crayon)
+* installing *binary* package ‘curl’ ...
+* DONE (curl)
+* installing *binary* package ‘fs’ ...
+* DONE (fs)
+* installing *binary* package ‘glue’ ...
+* DONE (glue)
+* installing *binary* package ‘jsonlite’ ...
+* DONE (jsonlite)
+* installing *binary* package ‘rappdirs’ ...
+* DONE (rappdirs)
+* installing *binary* package ‘rlang’ ...
+* DONE (rlang)
+* installing *binary* package ‘rprojroot’ ...
+* DONE (rprojroot)
+* installing *binary* package ‘rstudioapi’ ...
+* DONE (rstudioapi)
+* installing *binary* package ‘whisker’ ...
+* DONE (whisker)
+* installing *binary* package ‘withr’ ...
+* DONE (withr)
+* installing *binary* package ‘yaml’ ...
+* DONE (yaml)
+* installing *binary* package ‘askpass’ ...
+* DONE (askpass)
+* installing *binary* package ‘desc’ ...
+* DONE (desc)
+* installing *binary* package ‘lifecycle’ ...
+* DONE (lifecycle)
+* installing *binary* package ‘purrr’ ...
+* DONE (purrr)
+* installing *binary* package ‘openssl’ ...
+* DONE (openssl)
+* installing *binary* package ‘credentials’ ...
+* DONE (credentials)
+* installing *binary* package ‘httr’ ...
+* DONE (httr)
+* installing *binary* package ‘gert’ ...
+* DONE (gert)
+* installing *binary* package ‘gh’ ...
+* DONE (gh)
+* installing *binary* package ‘usethis’ ...
+* DONE (usethis)
+
+The downloaded source packages are in
+‘/tmp/RtmpEWTDbe/downloaded_packages’
+> library(usethis)
+> library(usethis)
+> use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+> install.packages("usethis")
+Error in install.packages : Updating loaded packages
+> library(usethis)
+> use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+> install.packages("usethis")
+Error in install.packages : Updating loaded packages
+> library(usethis)
+> use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+> git_sitrep()
+Git config (global)
+• Name: 'mattheweason'
+• Email: 'matthew.eason@som.umaryland.edu'
+• Global (user-level) gitignore file: <unset>
+  • Vaccinated: FALSE
+ℹ See `?git_vaccinate` to learn more
+ℹ Defaulting to 'https' Git protocol
+• Default Git protocol: 'https'
+• Default initial branch name: <unset>
+  GitHub
+• Default GitHub host: 'https://github.com'
+• Personal access token for 'https://github.com': <unset>
+  • To create a personal access token, call `create_github_token()`
+• To store a token for current and future use, call `gitcreds::gitcreds_set()`
+ℹ Read more in the 'Managing Git(Hub) Credentials' article:
+  https://usethis.r-lib.org/articles/articles/git-credentials.html
+Git repo for current project
+• Active usethis project: '/cloud/project'
+✖ The 'origin' remote is configured, but we can't determine its default branch.
+  Possible reasons:
+  - The remote repo no longer exists, suggesting the local remote should
+    be deleted.
+  - We are offline or that specific Git server is down.
+  - You don't have the necessary permission or something is wrong with
+your credentials.
+• Default branch: 'main'
+• Current local branch -> remote tracking branch:
+  'main' -> 'origin/main'
+GitHub remote configuration
+• Type = 'maybe_ours_or_theirs'
+• Host = 'https://github.com'
+• Config supports a pull request = NA
+• origin = 'mattheweason/cdabsR_Session4_Practice'
+• upstream = <not configured>
+  • Desc = 'origin' is a GitHub repo and 'upstream' is either not configured or is not a GitHub repo.
+
+We may be offline or you may need to configure a GitHub personal access
+token. `gh_token_help()` can help with that.
+
+Read more about what this GitHub remote configurations means at:
+  'https://happygitwithr.com/common-remote-setups.html'
+> install.packages("usethis")
+Error in install.packages : Updating loaded packages
+> library(usethis)
+> use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+> git_sitrep()
+Git config (global)
+• Name: 'mattheweason'
+• Email: 'matthew.eason@som.umaryland.edu'
+• Global (user-level) gitignore file: <unset>
+  • Vaccinated: FALSE
+ℹ See `?git_vaccinate` to learn more
+• Default Git protocol: 'https'
+• Default initial branch name: <unset>
+  GitHub
+• Default GitHub host: 'https://github.com'
+• Personal access token for 'https://github.com': <unset>
+  • To create a personal access token, call `create_github_token()`
+• To store a token for current and future use, call `gitcreds::gitcreds_set()`
+ℹ Read more in the 'Managing Git(Hub) Credentials' article:
+  https://usethis.r-lib.org/articles/articles/git-credentials.html
+Git repo for current project
+• Active usethis project: '/cloud/project'
+✖ The 'origin' remote is configured, but we can't determine its default branch.
+  Possible reasons:
+  - The remote repo no longer exists, suggesting the local remote should
+    be deleted.
+  - We are offline or that specific Git server is down.
+  - You don't have the necessary permission or something is wrong with
+your credentials.
+• Default branch: 'main'
+• Current local branch -> remote tracking branch:
+  'main' -> 'origin/main'
+GitHub remote configuration
+• Type = 'maybe_ours_or_theirs'
+• Host = 'https://github.com'
+• Config supports a pull request = NA
+• origin = 'mattheweason/cdabsR_Session4_Practice'
+• upstream = <not configured>
+  • Desc = 'origin' is a GitHub repo and 'upstream' is either not configured or is not a GitHub repo.
+
+We may be offline or you may need to configure a GitHub personal access
+token. `gh_token_help()` can help with that.
+
+Read more about what this GitHub remote configurations means at:
+  'https://happygitwithr.com/common-remote-setups.html'
+> 
+  > 
+  > create_github_token()
+• Call `gitcreds::gitcreds_set()` to register this token in the local Git credential store
+It is also a great idea to store this token in any password-management software that you use
+✔ Opening URL 'https://github.com/settings/tokens/new?scopes=repo,user,gist,workflow&description=DESCRIBE THE TOKEN\'S USE CASE'
+> # Add token to get credentials store
+  > gitcreds::gitcreds_set()
+
+
+? Enter password or token: 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
+-> Adding new credentials...
+-> Removing credentials from cache...
+-> Done.
+> install.packages("usethis")
+Error in install.packages : Updating loaded packages
+> library(usethis)
+> use_git_config(user.name = "mattheweason", user.email = "matthew.eason@som.umaryland.edu")
+> git_sitrep()
+Git config (global)
+• Name: 'mattheweason'
+• Email: 'matthew.eason@som.umaryland.edu'
+• Global (user-level) gitignore file: <unset>
+  • Vaccinated: FALSE
+ℹ See `?git_vaccinate` to learn more
+• Default Git protocol: 'https'
+• Default initial branch name: <unset>
+  GitHub
+• Default GitHub host: 'https://github.com'
+• Personal access token for 'https://github.com': '<discovered>'
+• GitHub user: 'mattheweason'
+• Token scopes: 'gist, repo, user, workflow'
+• Email(s): 'matthew.eason@som.umaryland.edu (primary)', '114687663+mattheweason@users.noreply.github.com'
+Git repo for current project
+• Active usethis project: '/cloud/project'
+• Default branch: 'main'
+• Current local branch -> remote tracking branch:
+  'main' -> 'origin/main'
+GitHub remote configuration
+• Type = 'ours'
+• Host = 'https://github.com'
+• Config supports a pull request = TRUE
+• origin = 'mattheweason/cdabsR_Session4_Practice' (can push)
+• upstream = <not configured>
+  • Desc = 'origin' is both the source and primary repo.
+
+Read more about the GitHub remote configurations that usethis supports at:
+  'https://happygitwithr.com/common-remote-setups.html'
